aaf71098b8b8eaafb24b692bbd844a57859b1af9
Kim naeun <eldpdl017@naver.com>
2026-01-27 17:23:17 +0900

    feat: 배치 미리보기 색 아웃라인으로 변경

--- 01_Scripts/Features/Placement/Application/PlaceableObject.cs
+++ 01_Scripts/Features/Placement/Application/PlaceableObject.cs
@@ -9,41 +9,40 @@ public class PlaceableObject : MonoBehaviour
     [SerializeField] private Int2 cellSize;
     public Int2 CellSize => cellSize;
 
-    private static readonly int BaseColorID = Shader.PropertyToID("_BaseColor");
-
     public void Bind(Placeable placeable)
     {
         this.type = placeable;
     }
 
+
+    private static readonly int OutlineColorID = Shader.PropertyToID("_OutlineColor");
+    private Color lastPreviewColor;
+
     public void SetPreviewColor(bool isAvailable, bool hasPlaced = false)
     {
         Color color = isAvailable ? Color.green : Color.red;
-        if (hasPlaced)
-        {
-            color = Color.white;
-        }
 
-        foreach (var renderer in renderers)
+        // 같은 색이면 작업 생략
+        if (lastPreviewColor == color) return;
+
+        for (int r = 0; r < renderers.Length; r++)
         {
+            var renderer = renderers[r];
             if (renderer == null) continue;
 
-            // 모든 머티리얼 인덱스에 대해 MaterialPropertyBlock 적용
-            var materials = renderer.sharedMaterials;
-
-            for (int i = 0; i < materials.Length; i++)
+            if (hasPlaced)
             {
-                var mat = materials[i];
-                var block = new MaterialPropertyBlock();
-                renderer.GetPropertyBlock(block, i);
-
-                if (mat != null)
-                {
-                    block.SetColor(BaseColorID, color);
-                }
-
-                renderer.SetPropertyBlock(block, i);
+                renderer.SetPropertyBlock(null);
+                continue;
             }
+
+            MaterialPropertyBlock mpb = new MaterialPropertyBlock();
+            mpb.SetColor(OutlineColorID, color);
+            mpb.SetFloat("_ID", 0);
+
+            renderer.SetPropertyBlock(mpb);
         }
+
+        lastPreviewColor = color;
     }
 }
